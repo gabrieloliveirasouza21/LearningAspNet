33e6288eea8d56188f2a9717fa28b71a06fc90f4
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2024-02-20 11:55:50 -0300

    Aula de status code

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) =>
+{
+    context.Response.StatusCode = 400;
+    await context.Response.WriteAsync("Hello");
+    await context.Response.WriteAsync("World");
+});
 
 app.Run();
